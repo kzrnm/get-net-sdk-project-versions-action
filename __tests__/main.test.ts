0e5d0b2b92e6ae8eff7242a43cc4e0adfb693e3f
naminodarie <gengesa@gmail.com>
2020-11-18 02:09:50 +0900

    fix test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,4 +1,3 @@
-import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 import * as os from 'os'
@@ -7,13 +6,10 @@ function run(filename: string): string[] {
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecSyncOptions = {
     env: {
-      'INPUT_PROJ-PATH': path.join(
-        __dirname,
-        'testdata',
-        filename
-      )
+      'INPUT_PROJ-PATH': path.join(__dirname, 'testdata', filename)
     }
   }
+  cp.exec(`node ${ip}`)
   return cp.execSync(`node ${ip}`, options).toString().split(os.EOL)
 }
 
@@ -26,7 +22,7 @@ test('Empty', () => {
       '::set-output name=package-version::1.0.0',
       '::set-output name=assembly-version::1.0.0',
       '::set-output name=file-version::1.0.0',
-      '::set-output name=informational-version::1.0.0',
+      '::set-output name=informational-version::1.0.0'
     ])
   )
 })
@@ -40,7 +36,7 @@ test('Verson', () => {
       '::set-output name=package-version::2.0.0-beta.1',
       '::set-output name=assembly-version::2.0.0',
       '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
@@ -54,7 +50,7 @@ test('VersonPrefix', () => {
       '::set-output name=package-version::3.4.2',
       '::set-output name=assembly-version::3.4.2',
       '::set-output name=file-version::3.4.2',
-      '::set-output name=informational-version::3.4.2',
+      '::set-output name=informational-version::3.4.2'
     ])
   )
 })
@@ -68,7 +64,7 @@ test('VersonSuffix', () => {
       '::set-output name=package-version::1.0.0-alpha.2',
       '::set-output name=assembly-version::1.0.0',
       '::set-output name=file-version::1.0.0',
-      '::set-output name=informational-version::1.0.0-alpha.2',
+      '::set-output name=informational-version::1.0.0-alpha.2'
     ])
   )
 })
@@ -82,7 +78,7 @@ test('VersonPrefix && VersonSuffix', () => {
       '::set-output name=package-version::2.0.0-beta.1',
       '::set-output name=assembly-version::2.0.0',
       '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
@@ -96,57 +92,55 @@ test('Verson && VersonPrefix && VersonSuffix', () => {
       '::set-output name=package-version::2.0.0-beta.1',
       '::set-output name=assembly-version::2.0.0',
       '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
 
-
-
 test('PackageVersion', () => {
-  expect(run('package.xml')).toEqual(
+  expect(run('package_version.xml')).toEqual(
     expect.arrayContaining([
       '::set-output name=version::2.0.0-beta.1',
       '::set-output name=version-prefix::2.0.0',
       '::set-output name=version-suffix::beta.1',
-      '::set-output name=package-version::2.0.0-beta.1',
+      '::set-output name=package-version::2.0.0-beta-1.1',
       '::set-output name=assembly-version::2.0.0',
       '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
 
 test('AssemblyVersion', () => {
-  expect(run('assembly.xml')).toEqual(
+  expect(run('assembly_version.xml')).toEqual(
     expect.arrayContaining([
       '::set-output name=version::2.0.0-beta.1',
       '::set-output name=version-prefix::2.0.0',
       '::set-output name=version-suffix::beta.1',
       '::set-output name=package-version::2.0.0-beta.1',
-      '::set-output name=assembly-version::2.0.0',
-      '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=assembly-version::2.0.0.227',
+      '::set-output name=file-version::2.0.0.227',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
 
 test('FileVersion', () => {
-  expect(run('file.xml')).toEqual(
+  expect(run('file_version.xml')).toEqual(
     expect.arrayContaining([
       '::set-output name=version::2.0.0-beta.1',
       '::set-output name=version-prefix::2.0.0',
       '::set-output name=version-suffix::beta.1',
       '::set-output name=package-version::2.0.0-beta.1',
       '::set-output name=assembly-version::2.0.0',
-      '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=file-version::2.0.0.21',
+      '::set-output name=informational-version::2.0.0-beta.1'
     ])
   )
 })
 
 test('informational', () => {
-  expect(run('file.xml')).toEqual(
+  expect(run('informational_version.xml')).toEqual(
     expect.arrayContaining([
       '::set-output name=version::2.0.0-beta.1',
       '::set-output name=version-prefix::2.0.0',
@@ -154,7 +148,7 @@ test('informational', () => {
       '::set-output name=package-version::2.0.0-beta.1',
       '::set-output name=assembly-version::2.0.0',
       '::set-output name=file-version::2.0.0',
-      '::set-output name=informational-version::2.0.0-beta.1',
+      '::set-output name=informational-version::v2 beta'
     ])
   )
 })
